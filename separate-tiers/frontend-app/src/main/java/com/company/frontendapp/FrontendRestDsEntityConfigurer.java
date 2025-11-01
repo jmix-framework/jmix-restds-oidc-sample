4a0be711f4402a3994b6f614bf6b2c3f0218f695
Konstantin Krivopustov <krivopustov@haulmont.com>
2025-11-01 18:38:10 +0400

    Temporary fix of jmix-framework/jmix#4852 and jmix-framework/jmix#4850

---
+++ separate-tiers/frontend-app/src/main/java/com/company/frontendapp/FrontendRestDsEntityConfigurer.java
@@ -0,0 +1,36 @@
+package com.company.frontendapp;
+
+import com.google.common.base.Strings;
+import io.jmix.core.JmixOrder;
+import io.jmix.core.MetadataMutationTools;
+import io.jmix.core.MetadataPostProcessor;
+import io.jmix.core.metamodel.model.Session;
+import io.jmix.flowuirestds.genericfilter.FilterConfiguration;
+import io.jmix.flowuirestds.settings.UserSettingsItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Temporary fix of https://github.com/jmix-framework/jmix/issues/4850
+ */
+@Component
+@Order(JmixOrder.LOWEST_PRECEDENCE - 100)
+public class FrontendRestDsEntityConfigurer implements MetadataPostProcessor {
+
+    @Value("${jmix.restds.ui-config-store:}")
+    private String uiConfigStore;
+
+    @Autowired
+    private MetadataMutationTools metadataMutationTools;
+
+    @Override
+    public void process(Session session) {
+        if (Strings.isNullOrEmpty(uiConfigStore))
+            return;
+
+        metadataMutationTools.setStore(session.getClass(UserSettingsItem.class), uiConfigStore);
+        metadataMutationTools.setStore(session.getClass(FilterConfiguration.class), uiConfigStore);
+    }
+}
